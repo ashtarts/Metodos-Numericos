4d2867c0966a8ae381fde05683a9fb490975c401
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-03-25 10:40:39 -0300

    Update and rename 4 interal (d).py to 4 integral (d).py

--- Lista de Exercícios com código (Integração Numérica)/4 integral (d).py
+++ Lista de Exercícios com código (Integração Numérica)/4 integral (d).py
@@ -1,7 +1,7 @@
 def f(x):
     return 1 / (1 + x)
 
-def sla(a, b, n):
+def t(a, b, n):
     h = (b - a) / n
     r = 0.5 * (f(a) + f(b))
     for i in range(1, n):
@@ -11,20 +11,28 @@ def sla(a, b, n):
 
 def m(a, b, epsilon):
     n = 1
-    i = sla(a, b, n)
+    i = t(a, b, n)
     p_i = i + 2 * epsilon
     while abs(i - p_i) > epsilon:
         n += 1
         p_i = i
-        i = sla(a, b, n)
+        i = t(a, b, n)
     return n
 
 a = 0
 b = 0.6
+n_4 = 4
+n_6 = 6
+
+i4 = t(a, b, n_4)
+i6 = t(a, b, n_6)
+
+print("4", i4)
+print("6", i6)
 epsilon = 1e-5
 
 min = m(a, b, epsilon)
-print( epsilon, min)
+print(   min)
 
-integral = sla(a, b, min)
+integral = t(a, b, min)
 print(integral)
