fc19249e553d0c782c59ea0ef34de46b54ebceae
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-03-25 10:25:40 -0300

    Update primeira integral (a).py

--- Lista de Exercícios com código (Integração Numérica)/primeira integral (a).py
+++ Lista de Exercícios com código (Integração Numérica)/primeira integral (a).py
@@ -3,22 +3,22 @@ import math
 def f(x):
     return math.exp(-x)
 
-def trapezoidal_rule(a, b, n):
+def t(a, b, n):
     h = (b - a) / n
-    result = 0.5 * (f(a) + f(b))
+    r = 0.5 * (f(a) + f(b))
     for i in range(1, n):
-        result += f(a + i * h)
-    result *= h
-    return result
+        r += f(a + i * h)
+    r *= h
+    return r
 
-def minimum_subintervals(a, b, epsilon):
+def m(a, b, epsilon):
     n = 1
-    integral_value = trapezoidal_rule(a, b, n)
-    prev_integral_value = integral_value + 2 * epsilon  # initialize with a value greater than epsilon
-    while abs(integral_value - prev_integral_value) > epsilon:
+    i = t(a, b, n)
+    piv = i + 2 * epsilon
+    while abs(i - piv) > epsilon:
         n += 1
-        prev_integral_value = integral_value
-        integral_value = trapezoidal_rule(a, b, n)
+        piv = i
+        i = t(a, b, n)
     return n
 
 
@@ -27,11 +27,11 @@ b = 2
 n_4 = 4
 n_6 = 6
 
-integral_value_4 = trapezoidal_rule(a, b, n_4)
-integral_value_6 = trapezoidal_rule(a, b, n_6)
-print("Integral using 4 divisions:", integral_value_4)
-print("Integral using 6 divisions:", integral_value_6)
+i4 = t(a, b, n_4)
+i6 = t(a, b, n_6)
+print("4:", i4)
+print("6:", i6)
 
-epsilon = 1e-5
-min_subintervals = minimum_subintervals(a, b, epsilon)
-print("Minimum subintervals to reach precision of", epsilon, ":", min_subintervals)
+e = 1e-5
+min = m(a, b, e)
+print( e, m)
