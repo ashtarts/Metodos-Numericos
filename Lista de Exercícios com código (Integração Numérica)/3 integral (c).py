24208a32568f4548cd8a7ca35f88f427ef0b72e9
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-03-25 10:38:41 -0300

    Update 3 integral (c).py

--- Lista de Exercícios com código (Integração Numérica)/3 integral (c).py
+++ Lista de Exercícios com código (Integração Numérica)/3 integral (c).py
@@ -3,7 +3,7 @@ import math
 def f(x):
     return 1 / math.sqrt(x)
 
-def sla(a, b, n):
+def t(a, b, n):
     h = (b - a) / n
     r = 0.5 * (f(a) + f(b))
     for i in range(1, n):
@@ -13,21 +13,30 @@ def sla(a, b, n):
 
 def m(a, b, epsilon):
     n = 1
-    i = sla(a, b, n)
+    i = t(a, b, n)
     p_i = i + 2 * epsilon
     while abs(i - p_i) > epsilon:
         n += 1
         p_i = i
-        i = sla(a, b, n)
+        i = t(a, b, n)
     return n
 
 
 a = 2
 b = 5
+n_4 = 4
+n_6 = 6
+
+i4 = t(a, b, n_4)
+i6 = t(a, b, n_6)
+
+print("4", i4)
+print("6", i6)
+
 epsilon = 1e-5
 
 min = m(a, b, epsilon)
-print("intervalos quant: ", min)
+print(min)
 
-integral = sla(a, b, min)
+integral = t(a, b, min)
 print(integral)
