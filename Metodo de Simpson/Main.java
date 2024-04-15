e2aac1de4fa3ba29dc03125b3dca67399aa6d127
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-04-15 10:39:08 -0300

    Update Main.java

--- Metodo de Simpson/Main.java
+++ Metodo de Simpson/Main.java
@@ -1,1 +1,41 @@
-ç
+import java.util.function.Function;
+
+public class Main {
+
+    public static double f(double x) {
+        return 1.0 / (1.0 + x);
+    }
+
+    public static double s(double a, double b, int n) {
+            if (n % 2 !=0) {
+                throw new IllegalArgumentException("par");
+            }
+        double h = (b - a) / n;
+        double sum1 = 0.0;
+        double sum2 = 0.0;
+
+        for (int i = 1; i < n; i++) {
+            double x = a + i * h;
+            if (i % 2 == 0) {
+                sum2 += f(x);
+            } else {
+                sum1 += f(x);
+            }
+        }
+
+        return (h / 3.0) * (f(a) + f(b) + 4.0 * sum1 + 2.0 * sum2);
+    }
+
+    public static void main(String[] args) {
+        double a = 0.6;
+        double b = 0.0;
+        int n = 1000;
+
+        try {
+            double i = s(a, b, n);
+            System.out.println("valor da integral " + i);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
